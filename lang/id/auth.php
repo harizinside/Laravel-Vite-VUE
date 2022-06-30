d09000366614d50f4d913a4ddcd969dcb4c89aa2
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2022-06-30 21:17:49 +0700

    add indonesian language

---
+++ lang/id/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
+return [
+    'failed'   => 'Identitas tersebut tidak cocok dengan data kami.',
+    'password' => 'Kata sandi yang dimasukkan salah.',
+    'booth'   => 'Surel dan Password yang diberikan salah, silakan coba lagi.',
+    'throttle' => 'Terlalu banyak upaya masuk. Silahkan coba lagi dalam :seconds detik.',
+    'transfer' => [
+        'mail_subject' => 'Verifikasi Karyawan NMW Klinik',
+        'mail_welcome'  => 'Selemat datang :name, di keluarga besar NMW Klinik',
+        'email_description' => 'Hai, sebelum anda menggunakan aplikasi versi terbaru. kami membutuhkan anda untuk mengisi dan melengkapi beberapa data.',
+        'email_button' => 'Lengkapi Data',
+        'email_footer' => 'Terimakasih',
+    ],
+];
---
+++ lang/id/email.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'welcome' => [
+		'mail_subject' => 'Selamat Bergabung',
+		'mail_header' => 'Hai :name',
+		'mail_description' => 'Senang berjumpa dengan anda, dengan akun ini anda dapat menelola seluruh aktifitas anda. termasuk untuk absebsi, form cuti, notifikasi dan lainnya, jika kamu mempunyai pertanyaan silakan hubungi kami di bawah ini',
+		'mail_footer' => 'Terimakasih.'
+	]
+];
