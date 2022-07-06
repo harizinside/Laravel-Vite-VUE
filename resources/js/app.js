df9ecbfae8200fd29a6d9bc33788216c36cbd1a8
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2022-07-06 09:32:15 +0700

    bring back vite to laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,19 @@
+import './bootstrap'
+import '../css/app.css'
+
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
-
-InertiaProgress.init()
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-  title: title => `${title} - My App`,
-  resolve: name => require(`./Pages/${name}`),
+  title: title => `${title}`,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
 })
+
+InertiaProgress.init()
